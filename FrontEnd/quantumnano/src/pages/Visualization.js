b5385fb0e8daf8366125986e33f214d2af724d0e
Jan Steinhauer <janst.geo@gmail.com>
2023-10-21 09:44:41 -0500

    file drag and drop in Visualization

--- FrontEnd/quantumnano/src/pages/Visualization.js
+++ FrontEnd/quantumnano/src/pages/Visualization.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../component/Banner';
 import SearchBar from '../component/SearchBar';
+import FileDropIn from '../component/FildDropIn';
 
 const Visualization = () => {
     return (
       <div className="visualization">
-        Visualization
+        <h1>Input your parameters to get a Visualisation 🧮</h1>
+        <FileDropIn></FileDropIn>
       </div>
     );
   };
