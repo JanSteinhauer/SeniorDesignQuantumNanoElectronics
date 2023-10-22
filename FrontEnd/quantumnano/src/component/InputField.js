c07b3d7cddc2accf8a094ee244fe5faf0a20ddb8
Jan Steinhauer <janst.geo@gmail.com>
2023-10-21 21:50:45 -0500

    input field finished

--- FrontEnd/quantumnano/src/component/InputField.js
+++ FrontEnd/quantumnano/src/component/InputField.js
@@ -2,68 +2,69 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 80%;
-  max-width: 300px;  // Reduced width
-  margin: 20px;
+  width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
 `;
 
 const Label = styled.label`
-  display: block;
+  display: flex;
+  align-items: center;
   font-size: 14px;
   margin-bottom: 5px;
   position: relative;
-  text-align: left;  // Added for left alignment
+  background-color: transparent;
+  justify-content: flex-start;
 `;
 
-const RequiredStar = styled.span`
+const InfoIcon = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  background-color: #ffffff;
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  text-align: center;
+  line-height: 18px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+`;
+
+const Asterisk = styled.span`
   color: red;
-  margin-left: 5px;
 `;
 
 const Input = styled.input`
   width: 100%;
-  padding: 10px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;  // Added to include padding and border in total width
-  margin-bottom: 5px;
+  padding: 8px;
+  box-sizing: border-box;
+  border-radius: 8px; // Added rounded corners to the input field.
 `;
 
-const HelpText = styled.span`
-  display: block;
+const HelpText = styled.p`
   font-size: 12px;
-  margin-bottom: 5px;
-  color: #888;  // Made text a bit lighter for subtlety
-`;
-
-const CharCount = styled.span`
-  display: block;
-  font-size: 12px;
-  text-align: right;
-  color: #888;  // Made text a bit lighter for subtlety
+  color: #666;
+  display: ${props => props.show ? 'block' : 'none'};
 `;
 
 const InputField = () => {
-  const [text, setText] = useState('');
-  const maxLength = 60;
+  const [showHelp, setShowHelp] = useState(false);
 
   return (
     <Container>
       <Label>
-        Label <RequiredStar>*</RequiredStar>
+        Label <Asterisk>*</Asterisk>
+        <InfoIcon onClick={() => setShowHelp(!showHelp)}>i</InfoIcon>
       </Label>
-      <Input 
-        type="text" 
-        value={text} 
-        onChange={e => setText(e.target.value)} 
-        maxLength={maxLength}
-        placeholder="Input Text"
-      />
-      <HelpText>Help Text</HelpText>
-      {/* <CharCount>{text.length}/{maxLength}</CharCount> */}
+      <Input type="text" placeholder="Input Text" />
+      <HelpText show={showHelp}>
+        Help Text
+      </HelpText>
     </Container>
   );
-};
+}
 
 export default InputField;
