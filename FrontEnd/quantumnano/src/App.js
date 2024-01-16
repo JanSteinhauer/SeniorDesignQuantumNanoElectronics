683b79a3d7b1c1b152ec91b862000abbd1c64782
Jan Steinhauer <janst.geo@gmail.com>
2024-01-16 10:35:06 -0600

    divided two features into two different Sites

--- FrontEnd/quantumnano/src/App.js
+++ FrontEnd/quantumnano/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './component/Navbar';
 import Home from './pages/Home';
 import Visualization from './pages/Visualization'; 
+import Script from './pages/Script'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/visualization" element={<Visualization />} />
+          <Route path="/script" element={<Script />} />
         </Routes>
       </div>
     </Router>
