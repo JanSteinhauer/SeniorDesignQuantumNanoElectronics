b2a3f45522c92aef449ccabebcff5c1a5fc94a9d
Jan Steinhauer <janst.geo@gmail.com>
2024-02-13 14:57:35 -0600

    add som

--- FrontEnd/quantumnano/src/pages/Peoples.js
+++ FrontEnd/quantumnano/src/pages/Peoples.js
@@ -15,13 +15,13 @@ const Peoples = () => {
   linkedInLink = "https://www.linkedin.com/in/jan-steinhauer-a18611206/ "
 />
 <People 
-  profileImageSrc="https://i.ibb.co/DYqx99f/Whats-App-Image-2024-02-12-at-21-54-44.jpg" 
+  profileImageSrc="https://i.ibb.co/JBQDqKR/default.jpg" 
   profileAltText="profile image"
-  name="Jan Steinhauer" 
-  role="Front End Developer" 
-  email = "janst.geo@gmail.com"
-  githubLink = "https://github.com/JanSteinhauer"
-  linkedInLink = "https://www.linkedin.com/in/jan-steinhauer-a18611206/ "
+  name="Som Wakdikar" 
+  role="Back End Developer" 
+  email = "somwakdikar@gmail.com"
+  githubLink = "https://github.com/somwakdikar"
+  linkedInLink = "https://www.linkedin.com/in/somwakdikar"
 />
 <People 
   profileImageSrc="https://i.ibb.co/DYqx99f/Whats-App-Image-2024-02-12-at-21-54-44.jpg" 
